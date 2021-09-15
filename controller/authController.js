c9be209b75d72be9ab047917f2cdea381e268bb0
edsan <jackwest415@gmail.com>
2021-09-15 04:39:54 +0300

    xau

--- controller/authController.js
+++ controller/authController.js
@@ -1,15 +1,56 @@
+const jwt = require("jsonwebtoken")
+const ErrorHandle = require('./../utils/errorApp')
 const User = require("./../model/usersModel")
 const catchAsync = require("./../utils/catchAsync")
 
+const jwtSign = id => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn : process.env.JWT_EXPIRES_IN
+    })
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        fullName : req.body.fullName,
+        email : req.body.email,
+        password : req.body.password,
+        passwordConfrim : req.body.passwordConfrim
+    });
+
+    const token = jwtSign(newUser._id)
 
     //Status 201 for created
     res.status(201).json({
         message: 'success',
         data: {
-            newUser
+            user : token
         }
     })
 }) 
+
+exports.login = catchAsync(async (req,res,next) => {
+    const {email, password} = req.body
+
+    // 1) check if email and password exists
+    if(!email || !password) {
+        return next(new ErrorHandle("invalid email or password", 400))
+    }
+
+    //2) check if user exists && password is correct
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user || !(await user.correctPassword(password, user.password))) {
+        return next(new ErrorHandle("invalid email or password", 400))
+    }
+
+    //3) If everything ok, send token to client
+    const token = jwtSign(user._id)
+
+    //Status 201 for created
+    res.status(201).json({
+        message: 'success',
+        data: {
+            user : token
+        }
+    })
+})
--- model/usersModel.js
+++ model/usersModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 const validator = require("validator")
 const { Schema } = mongoose
 
@@ -16,14 +17,43 @@ const userSchema = new Schema({
     password : {
         type : String,
         minLength : 8,
-        require: [true, "Please enter your password"]
+        require: [true, "Please enter your password"],
+        select : false
     },
     photo : {
         type : String
     },
     passwordConfrim : {
-        type : String
+        type : String,
+        validate : {
+            validator : function(el){
+            el === this.password
+        },
+        message : "passwords don't match"
+    }
     }
 })
+
+userSchema.pre("save", async function (next) {
+    //Only run this function if password is modified
+    if(!this.isModified("password")) return next()
+
+    //pre salting be4 
+    this.password = await bcrypt.hash(
+        this.password, 12
+    )
+
+    //clearing the saved password
+    this.passwordConfrim = undefined
+    next()
+})
+
+//init(available on all user docs) fn to compare candidatePassword(cP) and userPassword(uP)
+userSchema.methods.correctPassword = async function(cP, uP){
+    //returns true or false
+    return await bcrypt.compare(cP,uP)
+}
+
+
 
 module.exports = mongoose.model("User", userSchema)
